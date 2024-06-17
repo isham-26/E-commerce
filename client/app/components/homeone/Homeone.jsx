1d22471786b211abf756b0bc061bd232674954ed
isham-26 <ishamgupta711@gmail.com>
2024-06-17 06:16:45 +0530

    navbar complete

---
+++ client/app/components/homeone/Homeone.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Homeone = () => {
+  return (
+    <div className=''>
+      
+    </div>
+  )
+}
+
+export default Homeone
---
+++ client/app/page.js
@@ -0,0 +1,10 @@
+import Homeone from "./components/homeone/Homeone";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Homeone/>
+    </div>
+  );
+}
