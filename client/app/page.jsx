040edb3f16139d30beeee2413bc407378eaef0e2
isham-26 <ishamgupta711@gmail.com>
2024-06-22 20:38:36 +0530

    footer change

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -4,16 +4,18 @@ import NewCollection from "./components/newCollection/NewCollection";
 import NewLetter from "./components/newLetter/NewLetter";
 import Offers from "./components/offers/Offers";
 import Popular from "./components/popular/Popular";
+import Slider from "./components/slider/Slider";
 
 
 export default function Home() {
   return (
     <div className="">
-      <Homeone/>
+      <Homeone/>  {/* <Slider/> */}
       <Popular/>
       <Offers/>
       <NewCollection/>
       <NewLetter/>
+      
     </div>
   );
 }
