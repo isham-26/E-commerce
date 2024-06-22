7f0759d35229eb180d7502d63d3a19abca174462
isham-26 <ishamgupta711@gmail.com>
2024-06-22 20:41:38 +0530

    slider

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -4,7 +4,6 @@ import NewCollection from "./components/newCollection/NewCollection";
 import NewLetter from "./components/newLetter/NewLetter";
 import Offers from "./components/offers/Offers";
 import Popular from "./components/popular/Popular";
-import Slider from "./components/slider/Slider";
 
 
 export default function Home() {
